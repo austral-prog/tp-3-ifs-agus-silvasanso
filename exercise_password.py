8c7ab881952aee6acd2aa96a13af6fbd9ba68009
Agus Silva Sansó <silvasansoagustin@gmail.com>
2026-03-31 10:45:25 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -23,7 +23,6 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
-    pass
     password= input()
     longitud= len(password) >= 8
     numero= "0" in password or "1" in password or "2" in password or "3" in password or "4" in password or "5" in password or "6" in password or "7" in password or "8" in password or "9" in password
